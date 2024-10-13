1f70214fb35b8137a725e951a9a4259385320d81
Agniva Banerjee <agniva.banerjee@harness.io>
2024-10-14 01:21:55 +0530

    reformatting

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from "react"
 import Contact from "./Contact"
 import CatData  from "./CatData"
-const ContactCatData=CatData.map(cat=> <Contact key={cat.id} img={cat.img} name={cat.name} phone={cat.phone} email={cat.email} />)
+const ContactCatData=CatData.map(cat=> 
+   <Contact 
+       img={cat.img} 
+       name={cat.name} 
+       phone={cat.phone} 
+       email={cat.email} 
+    />
+)
 function App() {
     return (
         <div className="contacts">
